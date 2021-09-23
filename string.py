d385e682f3e3e274fef981d21ebf819b13bb47bf
AdamZullowa06 <adamzullowa06@gmail.com>
2021-09-23 23:23:04 +0700

    add string

---
+++ string.py
@@ -0,0 +1,11 @@
+nama = "Adam Zullowa"
+email = "adamzullowa06@gmail.com"
+alamat = "jalan kampung pulo"
+
+nama_depan = "Adam"
+nama_belakang = "Zullowa"
+nama_lengkap = nama_depan + " " + nama_belakang
+
+print(nama_lengkap)
+print(email)
+print(alamat)
