c78ce30dfbbf6fb24a23cea946c0024287c77860
AdamZullowa06 <adamzullowa06@gmail.com>
2021-09-27 21:13:07 +0700

    adding operator perbandingan

---
+++ operator_perbandingan.py
@@ -0,0 +1,16 @@
+# Operator perbandingan
+# 
+# > lebih dari
+# < kurang dari
+# >= lebih dari sama dengan
+# <= kurang dari sama dengan
+# == sama dengan
+# != tidak sama dengan 
+
+print(1 <= 1)
+print(10 == 10)
+print(10 != 9)
+
+# perbandingan string
+print("adam" == "adam")
+print("adam" != "adam")
