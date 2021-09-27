2faad6f89da6cf0ea56e7fd1aafe330d4b0e3394
AdamZullowa06 <adamzullowa06@gmail.com>
2021-09-27 21:13:24 +0700

    adding if statement

---
+++ if.py
@@ -0,0 +1,13 @@
+# python if statement
+
+orang = 1000
+zombie = 100
+
+if orang < zombie :
+    print("kabur, dunia mau kiamat")
+
+if orang > zombie :
+    print("ayo basmi zombie")
+
+if orang == zombie :
+    print("mari berperang")
