45c5516a734684e1646ced9e9dec130045d967e7
AdamZullowa06 <adamzullowa06@gmail.com>
2021-09-28 23:20:05 +0700

    adding elif

---
+++ elif.py
@@ -0,0 +1,15 @@
+# python elif
+
+menu_pilihan = input("Silahkan pilih menu [1-3] : ")
+
+if menu_pilihan == "1" :
+    print("Anda memilih menu 1")
+
+elif menu_pilihan == "2" :
+    print("Anda memilih menu 2")
+
+elif menu_pilihan == "3" :
+    print("Anda memilih menu 3")
+
+else :
+    print("Menu tidak tersedia")
