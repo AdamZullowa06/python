6280b2ec4ec061443e57e734f5c100e137061d12
AdamZullowa06 <adamzullowa06@gmail.com>
2021-09-29 23:48:03 +0700

    editing list

---
+++ list_edit.py
@@ -0,0 +1,16 @@
+# membuat list
+nama = ["eko","budi","andi"]
+
+# menambah data ke list
+nama.append("adam")
+
+# mengakses list
+nama[0]
+
+# menhapus data dari list
+nama.remove("budi")
+
+# mengubah data di list
+nama[0] = "Sasa"
+
+print(nama)
