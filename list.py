41b6cc9c8d3db2b009f9cea6c13ab703a84ba917
AdamZullowa06 <adamzullowa06@gmail.com>
2021-09-29 23:47:39 +0700

    adding list

---
+++ list.py
@@ -0,0 +1,15 @@
+# python list
+
+nama = ["eko", "budi", "andi"]
+nama.append("adam")
+
+print(nama[0])
+print(nama[1])
+print(nama[2])
+print(nama[3])
+
+print(len(nama))
+
+nama.remove("adam")
+
+print(nama)
