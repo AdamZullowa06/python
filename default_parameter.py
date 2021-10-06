2f49af1191b61458a12a0db0d2068f85bafdbb6c
AdamZullowa06 <adamzullowa06@gmail.com>
2021-10-06 22:02:44 +0700

    adding default parameter

---
+++ default_parameter.py
@@ -0,0 +1,7 @@
+# python default argument value
+
+def say_hello(first_name = "", last_name = "") :
+    print(f"hello {first_name} {last_name}")
+
+say_hello("adam")
+say_hello(last_name="zullowa")
