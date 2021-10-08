61cf2a2decc04d3fb4e0a322f4881e60c7754264
AdamZullowa06 <adamzullowa06@gmail.com>
2021-10-08 15:15:34 +0700

    adding module and function

---
+++ function.py
@@ -0,0 +1,10 @@
+# python module dan function
+
+def say_hello(nama) :
+    return f"Hello {nama}"
+
+def total(*list_angka) :
+    hasil = 0
+    for data in list_angka :
+        hasil = hasil + data
+    return hasil
---
+++ module.py
@@ -0,0 +1,11 @@
+# python module
+
+# import function
+from function import say_hello
+from function import total
+
+hello = say_hello("Eko")
+print(hello)
+
+hasil = total(1,2,3,4,5)
+print(hasil)
